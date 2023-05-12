43b21698bf1e86ce01aa6b0772203a146c58e1ab
Manthan-mishra <manthan9481147615@gmail.com>
2023-05-12 20:57:03 +0530

    navbar control feature

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,12 +11,34 @@ import logo from "../../assets/movix-logo.svg";
 
 const Header = () => {
   const [show, setShow] = useState("top");
-  // const [lastScrollY, setLastScrollY] = useState(0);
+  const [lastScrollY, setLastScrollY] = useState(0);
   const [mobileMenu, setMobileMenu] = useState(false);
   const [query, setQuery] = useState("");
   const [showSearch, setShowSearch] = useState("");
   const navigate = useNavigate();
   // const location = useLocation();
+
+  const controlNavbar = () => {
+    console.log(window.scrollY);
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("hide");
+      } else {
+        setShow("show");
+      }
+    }
+     else{
+
+     } 
+      setLastScrollY(window.scrollY);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, [lastScrollY]);
 
   const searchQueryHandler = (e) => {
     if (e.key === "Enter" && query.length > 0) {
